7f7abc5350267e1fe9c51f2bdb40e8727c974b1f
Joao Pedro <joaopedroricarte@hotmail.com>
2019-09-21 01:17:42 -0300

    feito a opcao de editar

--- crud.js
+++ crud.js
@@ -1,5 +1,5 @@
 
-function valor1(){
+function adicionar(){
     var table = document.getElementById("tabela");   
     var row = table.insertRow();
     
@@ -16,7 +16,7 @@ function valor1(){
     c.innerHTML = document.getElementById("valor").value;
     d.innerHTML = document.getElementById("stack").value;
     e.innerHTML = document.getElementById("dias").value;
-    edit.innerHTML = '<button onclick="editar()" id="botao-edit">EDITAR</button>'
+    edit.innerHTML = '<button onclick="editar(this)" id="botao-edit">EDITAR</button>';
     del.innerHTML = '<button onclick="deletar(this)" id="botao-del">DELETAR</button>';
     
     document.getElementById("desenvolvedor").value = '';
@@ -29,18 +29,42 @@ function valor1(){
 }
 
 function deletar(td){
-    selectedRow = td.parentElement.parentElement;
+    selectedRow = td.parentElement.parentElement; //sobe uma <div> a cada parent element 
     var table = document.getElementById("tabela");   
     table.deleteRow(selectedRow.rowIndex);
+    
 }
 
-function editar(){
+function editar(td){
     
+    selectedRow = td.parentElement.parentElement;
+
+    document.getElementById("desenvolvedor").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("projeto").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("valor").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("stack").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("dias").value = selectedRow.cells[4].innerHTML;
     
-    a = document.getElementById("desenvolvedor").value;
-    b = document.getElementById("projeto").value;
-    c = document.getElementById("valor").value;
-    d = document.getElementById("stack").value;
-    e = document.getElementById("dias").value;
+    document.getElementById("edit-confirm").style.visibility = 'visible'
+
+}
+function editconfirm(){  
+   
+    selectedRow.cells[0].innerHTML = document.getElementById("desenvolvedor").value;
+    selectedRow.cells[1].innerHTML = document.getElementById("projeto").value;    
+    selectedRow.cells[2].innerHTML = document.getElementById("valor").value;
+    selectedRow.cells[3].innerHTML = document.getElementById("stack").value;
+    selectedRow.cells[4].innerHTML = document.getElementById("dias").value;
+    
+    document.getElementById("edit-confirm").style.visibility = 'hidden'
+
+    document.getElementById("desenvolvedor").value = '';
+    document.getElementById("projeto").value = '';
+    document.getElementById("valor").value = '';
+    document.getElementById("stack").value = '';
+    document.getElementById("dias").value = '';
+
+    event.preventDefault();
+  
 
 }
